1071c8ec9331695c7912877b87fd0f6a409b0e38
Michael <email": null>
2015-10-30 17:40:28 -0400

    This is a huge commit doing bunches of stuff just oodles

--- vampires-deck.rb
+++ vampires-deck.rb
@@ -1,20 +1,52 @@
-
 class Deck
+  attr_accessor :cards
+
+  def initialize
+    @cards = []
+    puts "You ever use flash cards before? Just give the word for each definition"
+    load_txt_file
+  end
+
+  def load_txt_file
+    @cards = File.open('definitions.txt').readlines
+    deletes_space
+  end
+
+  def deletes_space
+    @cards.reject!{|x|x=="\n"}
+    turn_into_hash
+  end
+
+  def turn_into_hash
+    @cards=Hash[*cards]
+    print_definition
+  end
+
+  def pick_card
+    @cards.keys.sample
+  end
+
+  def pick_new_card
+    pick_card
+    print_definition
+  end
+
   def print_definition
-    puts "Definition:\n" + pick_card.definition
+    definition = pick_card
+    puts "Definition:\n#{definition}"
     user_input
+    compare
   end
 
   def user_input
-    input = gets.chomp
-    compare(input)
+    gets.chomp
   end
 
-  def compare(input)
-    if input == pick_card.question
+  def compare
+    if user_input == @cards[pick_card]
       output_correct
-      pick_card
-    elsif input == 'exit'
+      pick_new_card
+    elsif user_input == 'exit'
       puts "Quitter."
     else
       output_incorrect
@@ -27,7 +59,8 @@ class Deck
   end
 
   def output_incorrect
-    puts "You suck! If this were actual Jeopardy Trebeck would have laughed you out of town. Try again"
+    puts "You suck! If this were actual Jeopardy Trebeck would have laughed you out of town`"
   end
 end
 
+Deck.new
