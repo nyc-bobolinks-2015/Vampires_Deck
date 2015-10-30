c98a480c50590eb2451ef9c8b012071b456bd64a
Michael <email": null>
2015-10-30 12:05:22 -0400

    Add compare method and Card class

---
+++ vampires-deck.rb
@@ -0,0 +1,34 @@
+class Deck
+
+
+  def compare
+    if user_input == pick_card.question
+      output_correct
+      pick_card
+    elsif user_input == 'exit'
+      puts "Quitter."
+    else
+      output_incorrect
+      user_input
+    end
+
+  end
+
+  def output_correct
+    puts "Wow you must be some kind of genius...Next question!"
+  end
+
+  def output_incorrect
+    puts "You suck! If this were actual Jeopardy Trebeck would have laughed you out of town"
+  end
+end
+
+class Card
+  attr_reader :definition, :question
+
+  def initialize(args)
+    @definition = args[:defintion]
+    @question = args[:question]
+  end
+
+end
