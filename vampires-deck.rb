1e865f72ae1d8538d90313f02c36a5f883058d4e
Awilda <awildacantres@gmail.com>
2015-10-30 14:16:26 -0400

    Update and test definition method and input method

--- vampires-deck.rb
+++ vampires-deck.rb
@@ -1,11 +1,29 @@
+# class Test
+#test
+# 	def initialize
+# 		@defintion = {"I'm a definition." => "I'm an answer", "I'm a definition." => "I'm an answer"}
+# 		print_definition
+# 	end
 
-def print_definition
-	definition = pick_card.each_key {|def| puts key}
-	#user_input
+	def print_definition
+		puts "definition"
+		# @definition = pick_card.each_key {|def_key| puts def_key}
+		@defintion.each {|def_key| puts def_key}
+		puts
+		user_input
+	end
+
+	def user_input
+		input = ""
+		@definition #maybe I should take this out
+		input = gets.chomp
+		yay
+	end
+#test
+	# def yay
+	# 	puts "You did it!!!"
+	# end
+
 end
 
-def user_input
-	input = ""
-	definition #maybe I should take this out
-	input = gets.chomp
-end
+let = Test.new()
